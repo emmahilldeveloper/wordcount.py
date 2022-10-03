a4ea1bddc659dc0c88774b8855817f792c7a71e5
emmahilldeveloper <emhillx693@gmail.com>
2022-10-03 14:57:48 -0500

    Removed comments.

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,10 @@
 """Count words in file."""
 
-# with open('test.txt') as file:
-#     phrase = file.read()
-
 phrase = open("test.txt")
 
 def word_count(phrase):
 
     word_counts = {}
-    #list_of_strings = []
-
-    # for line in phrase:
-    #     list_of_strings.append(line.split(" "))
 
     for line in phrase:
 
@@ -22,5 +15,3 @@ def word_count(phrase):
             word_counts[word] = word_counts.get(word, 0) + 1
 
     return word_counts
-
-    #print(phrase)
